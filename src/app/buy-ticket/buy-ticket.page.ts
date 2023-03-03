c83b82e0fe927754d091965cc129a84d31bf71ae
Prottoss <prottoss@hotmail.co.uk>
2023-03-03 17:36:20 +0000

    implemented ticket time picker

--- src/app/buy-ticket/buy-ticket.page.ts
+++ src/app/buy-ticket/buy-ticket.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { PickerController } from '@ionic/angular';
 
 @Component({
   selector: 'app-buy-ticket',
@@ -7,9 +9,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuyTicketPage implements OnInit {
 
-  constructor() { }
+  currentTime = new Date();
+  parkingTime: any;
+  expiryTime: any;
+  constructor(public userService: UserService, private pickerCtrl: PickerController ) { }
 
   ngOnInit() {
+  }
+
+  async openPicker() {
+    const picker = await this.pickerCtrl.create({
+      columns: [
+        {
+          name: 'timeSelected',
+          options: [
+            {
+              text: '1 Hour',
+              value: 1,
+            },
+            {
+              text: '2 Hours',
+              value: 2,
+            },
+            {
+              text: '3 Hours',
+              value: 3,
+            },
+            {
+              text: '4 Hours',
+              value: 4,
+            },
+            {
+              text: '5 Hours',
+              value: 5,
+            },
+            {
+              text: '6 Hours',
+              value: 6,
+            },
+            {
+              text: '7 Hours',
+              value: 7,
+            },
+            {
+              text: '8 Hours',
+              value: 8,
+            },
+          ],
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+        {
+          text: 'Confirm',
+          handler: (value) => {
+            this.incrementTime(value.timeSelected.value);
+          },
+        },
+      ],
+    });
+
+    await picker.present();
+  }
+
+  async incrementTime(time:any) {
+    const incrementMilliseconds = time * 60 * 60 * 1000; // convert hours to milliseconds
+    const newTime = new Date(this.currentTime.getTime() + incrementMilliseconds);
+    this.expiryTime = newTime;
   }
 
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
 import { Auth } from '@angular/fire/auth';
-import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +10,7 @@ export class UserService {
 
   constructor(private db: AngularFireDatabase, private auth: Auth ) { }
 
+  //User functions
   createUser(uid:any, email:any){
     this.db.object("users/"+uid).set({
       uid: uid,
@@ -27,13 +27,32 @@ export class UserService {
     return this.db.list("users");
   }
 
+  //User vehicle functions
   addVehicleToUser(reg:any){
     const user = this.auth.currentUser;
-    this.db.list("users/"+user?.uid+"/vehicles/"+reg).set("reg",reg);
+    this.db.object("users/"+user?.uid+"/vehicles/"+reg).set({
+      reg:reg
+    });
   }
 
   getVehicles(){
     const user = this.auth.currentUser;
     return this.db.list("users/"+user?.uid+"/vehicles");
+  }
+
+  //User ticket functions
+  addTicketToUser(ticket:any){
+    const user = this.auth.currentUser;
+    this.db.object("users/"+user?.uid+"/tickets/"+ticket.id).set({
+      id: ticket.id,
+      ticketStart: ticket.start,
+      ticketEnd: ticket.end,
+      ticketDuration: ticket.duration,
+      price: ticket.price
+    });
+  }
+
+  buyTicket(){
+    
   }
 }
