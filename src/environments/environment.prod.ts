ad613dbbebf98608a9deac0c59a5c6f4987465b6
Prottoss <prottoss@hotmail.co.uk>
2023-03-12 10:23:51 +0000

    addedd android

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDCGQKwhDCzn9fkW_3Hi4u014aOj6P7IUI",
+    authDomain: "parkit-b3674.firebaseapp.com",
+    projectId: "parkit-b3674",
+    databaseURL: "https://parkit-b3674-default-rtdb.europe-west1.firebasedatabase.app/",
+    storageBucket: "parkit-b3674.appspot.com",
+    messagingSenderId: "618777176789",
+    appId: "1:618777176789:web:fc11f5b2cb20e80135905c",
+    measurementId: "G-HRXYM7P5LR"
+  }
 };
